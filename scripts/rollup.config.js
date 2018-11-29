e3b9170d9c7293722fbd3e860c31bb4321f803be
pimlie <pimlie@hotmail.com>
2018-11-29 19:01:45 +0100

    fix: umd build

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -12,66 +12,55 @@ const base = path.resolve(__dirname, '..')
 const lib = path.resolve(base, 'lib')
 const dist = path.resolve(base, 'dist')
 
+const externalExcludes = [
+  'lodash.throttle',
+  'lodash.snakecase'
+]
+
+const baseConfig = {
+  input: path.resolve(lib, 'index.js'),
+  external: Object.keys(dependencies),
+  plugins: [
+    vuePlugin({ css: false }),
+    nodeResolve({ external: ['vue'], preferBuiltins: true }),
+    commonJS(),
+    buble({ objectAssign: 'Object.assign' }),
+    terser({ sourcemap: true })
+  ]
+}
+
 // Ensure dist directory exists
 if (!fs.existsSync(dist)) {
   fs.mkdirSync(dist)
 }
 
 export default [{
-  input: path.resolve(lib, 'index.js'),
+  ...baseConfig,
   output: {
-    name: camelCase(name),
     format: 'cjs',
-    globals: {
-      ...dependencies
-    },
+    name: camelCase(name),
     file: path.resolve(dist, name + '.common.js'),
     sourcemap: true
   },
-  external: Object.keys(dependencies),
-  plugins: [
-    vuePlugin({ css: false }),
-    nodeResolve({ external: ['vue'], preferBuiltins: true }),
-    commonJS(),
-    buble({ objectAssign: 'Object.assign' }),
-    terser({ sourcemap: true })
-  ]
+  external: Object.keys(dependencies).filter(
+    dep => externalExcludes.indexOf(dep) === -1
+  )
 }, {
-  input: path.resolve(lib, 'index.js'),
+  ...baseConfig,
   output: {
-    name,
-    format: 'es',
-    globals: {
-      ...dependencies
-    },
-    file: path.resolve(dist, name + '.esm.js'),
-    sourcemap: true
-  },
-  external: Object.keys(dependencies),
-  plugins: [
-    vuePlugin({ css: false }),
-    nodeResolve({ external: ['vue'], preferBuiltins: true }),
-    commonJS(),
-    buble({ objectAssign: 'Object.assign' }),
-    terser({ sourcemap: true })
-  ]
-}, {
-  input: path.resolve(lib, 'index.js'),
-  output: {
-    name: camelCase(name),
     format: 'umd',
-    globals: {
-      ...dependencies
-    },
+    name: camelCase(name),
     file: path.resolve(dist, name + '.js'),
     sourcemap: true
   },
-  external: Object.keys(dependencies),
-  plugins: [
-    vuePlugin({ css: false }),
-    nodeResolve({ external: ['vue'], preferBuiltins: true }),
-    commonJS(),
-    buble({ objectAssign: 'Object.assign' }),
-    terser({ sourcemap: true })
-  ]
+  external: Object.keys(dependencies).filter(
+    dep => externalExcludes.indexOf(dep) === -1
+  )
+}, {
+  ...baseConfig,
+  output: {
+    format: 'es',
+    file: path.resolve(dist, name + '.esm.js'),
+    sourcemap: true
+  }
 }]
